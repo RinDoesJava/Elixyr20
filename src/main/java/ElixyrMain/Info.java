f340c1298eda0ae827c6d2c9e23572ab4c56376f
RinDoesJava <43514989+RinDoesJava@users.noreply.github.com>
2018-09-28 10:34:34 +0200

    Fixed prefix

--- src/main/java/ElixyrMain/Info.java
+++ src/main/java/ElixyrMain/Info.java
@@ -6,7 +6,7 @@ public class Info {
 
     public static String TOKEN = "NDg3Mjg4Njc3Njc3MDA2ODQ4.Dn_LLw.MPKfidJDdFsuWJkMYe4WhG9Fsj0";
 
-    public static String PREFIX = "dev;";
+    public static String PREFIX = "ely;";
 
     public static String APIKEY = "AIzaSyDiMW7B6ANsGPdHluASNZlUBlpHtnco8ow";
 }
