18a9fcad05a720e573f0a0966e20a70f11857533
RinDoesJava <43514989+RinDoesJava@users.noreply.github.com>
2018-09-30 23:41:42 +0200

    Got rid of my token in Info lmao

--- src/main/java/ElixyrMain/Info.java
+++ src/main/java/ElixyrMain/Info.java
@@ -4,7 +4,7 @@ public class Info {
 
     public static String VERSION = "2.0.0";
 
-    public static String TOKEN = "NDg3Mjg4Njc3Njc3MDA2ODQ4.Dn_LLw.MPKfidJDdFsuWJkMYe4WhG9Fsj0";
+    public static String TOKEN = "lol no";
 
     public static String PREFIX = "ely;";
 
